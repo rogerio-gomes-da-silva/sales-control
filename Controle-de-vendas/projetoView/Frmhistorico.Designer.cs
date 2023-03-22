594a7cb50ac73563c6cf8ff0e9880fd0ed242dc6
Rogério Gomes <rogerim00@hotmail.com>
2023-03-22 19:37:22 -0300

    Implementing history screen / Implementando tela de histórico

---
+++ Controle-de-vendas/projetoView/Frmhistorico.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Controle_de_vendas.projetoView
+{
+    partial class Frmhistorico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frmhistorico));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnpesquisarpordata = new System.Windows.Forms.Button();
+            this.dtFim = new System.Windows.Forms.DateTimePicker();
+            this.dtInicio = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabelaHistorico = new System.Windows.Forms.DataGridView();
+            this.colCod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colObs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaHistorico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Histórico de vendas";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 74);
+            this.panel1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnpesquisarpordata);
+            this.groupBox1.Controls.Add(this.dtFim);
+            this.groupBox1.Controls.Add(this.dtInicio);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(30, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(741, 83);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Consulta";
+            // 
+            // btnpesquisarpordata
+            // 
+            this.btnpesquisarpordata.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnpesquisarpordata.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpesquisarpordata.ForeColor = System.Drawing.Color.White;
+            this.btnpesquisarpordata.Image = ((System.Drawing.Image)(resources.GetObject("btnpesquisarpordata.Image")));
+            this.btnpesquisarpordata.Location = new System.Drawing.Point(645, 19);
+            this.btnpesquisarpordata.Name = "btnpesquisarpordata";
+            this.btnpesquisarpordata.Size = new System.Drawing.Size(80, 50);
+            this.btnpesquisarpordata.TabIndex = 27;
+            this.btnpesquisarpordata.UseVisualStyleBackColor = false;
+            // 
+            // dtFim
+            // 
+            this.dtFim.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold);
+            this.dtFim.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFim.Location = new System.Drawing.Point(431, 24);
+            this.dtFim.Name = "dtFim";
+            this.dtFim.Size = new System.Drawing.Size(200, 32);
+            this.dtFim.TabIndex = 4;
+            // 
+            // dtInicio
+            // 
+            this.dtInicio.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold);
+            this.dtInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtInicio.Location = new System.Drawing.Point(116, 24);
+            this.dtInicio.Name = "dtInicio";
+            this.dtInicio.Size = new System.Drawing.Size(200, 32);
+            this.dtInicio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(330, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data final";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(6, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data início";
+            // 
+            // tabelaHistorico
+            // 
+            this.tabelaHistorico.AllowUserToAddRows = false;
+            this.tabelaHistorico.AllowUserToDeleteRows = false;
+            this.tabelaHistorico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabelaHistorico.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCod,
+            this.colData,
+            this.colCliente,
+            this.colTotal,
+            this.colObs});
+            this.tabelaHistorico.Location = new System.Drawing.Point(30, 181);
+            this.tabelaHistorico.Name = "tabelaHistorico";
+            this.tabelaHistorico.ReadOnly = true;
+            this.tabelaHistorico.Size = new System.Drawing.Size(741, 257);
+            this.tabelaHistorico.TabIndex = 6;
+            // 
+            // colCod
+            // 
+            this.colCod.HeaderText = "Código";
+            this.colCod.Name = "colCod";
+            this.colCod.ReadOnly = true;
+            // 
+            // colData
+            // 
+            this.colData.HeaderText = "Data da venda";
+            this.colData.Name = "colData";
+            this.colData.ReadOnly = true;
+            this.colData.Width = 150;
+            // 
+            // colCliente
+            // 
+            this.colCliente.HeaderText = "Cliente";
+            this.colCliente.Name = "colCliente";
+            this.colCliente.ReadOnly = true;
+            this.colCliente.Width = 200;
+            // 
+            // colTotal
+            // 
+            this.colTotal.HeaderText = "Total";
+            this.colTotal.Name = "colTotal";
+            this.colTotal.ReadOnly = true;
+            // 
+            // colObs
+            // 
+            this.colObs.HeaderText = "Observação";
+            this.colObs.Name = "colObs";
+            this.colObs.ReadOnly = true;
+            this.colObs.Width = 150;
+            // 
+            // Frmhistorico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabelaHistorico);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Frmhistorico";
+            this.Text = "Histórico de vendas";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tabelaHistorico)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtFim;
+        private System.Windows.Forms.DateTimePicker dtInicio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView tabelaHistorico;
+        private System.Windows.Forms.Button btnpesquisarpordata;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colObs;
+    }
+}
